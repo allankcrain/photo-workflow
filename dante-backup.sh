cf81e0643ef604ccf771453f8f4a80d01fca55c2
Allan Crain <allankcrain@gmail.com>
2020-09-17 22:13:00 -0500

    Reduced the verbosity of the backup rsync; added a timestamp to the sanity check file so I can tell backups are happening

--- dante-backup.sh
+++ dante-backup.sh
@@ -5,7 +5,8 @@ PBK=/mnt/pictures-bak/allanc/Pictures
 # Check to make sure that both the Pictures and Pictures-bak raids are mounted
 if [ -f $PIX/raid-sanity-main ]; then
   if [ -f $PBK/raid-sanity-backup ]; then
-    rsync -v -a --delete-after /mnt/pictures/ /mnt/pictures-bak/
+    echo "Last backup attempt: "`date` >$PIX/raid-sanity-main
+    rsync -a --delete-after /mnt/pictures/ /mnt/pictures-bak/
     mv $PBK/raid-sanity-main $PBK/raid-sanity-backup
   else
     echo "Dang, backup Pictures RAID isn't mounted?"
